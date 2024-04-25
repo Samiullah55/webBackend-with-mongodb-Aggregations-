5a0c95f2b53f732e25b3603f314ac22261e84e8b
Sami Ullah <samibangash2018@gmail.com>
2024-04-25 12:15:49 +0500

    utils setup for better error handling

--- server.js
+++ server.js
@@ -2,13 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db/dbConnection.js";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const PORT = process.env.PORT || 8001;
 
 const app = express();
+
 app.use(cors());
-connectDB();
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error connecting database ", err);
+  });
