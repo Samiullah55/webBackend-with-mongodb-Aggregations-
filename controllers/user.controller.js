af3d387b8f1f2cf58734bd593220f3ebaceb204a
Sami Ullah <samibangash2018@gmail.com>
2024-04-26 13:03:34 +0500

    register controller setup

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,9 +1,64 @@
 import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/apiError.js";
+import ApiResponse from "../utils/apiResponse.js";
+import User from "../models/user.model.js";
+import uploadOnCloudinary from "../utils/cloudinary.js";
 
 const registerController = asyncHandler(async (req, res) => {
-  return res.status(200).json({
-    message: "ok",
+  // get user details from frontend
+  // validating data
+  // check for existince of user
+  // check for files
+  // upload files to cloud
+  // create user
+  // exclude sensitive info from response
+  // check for user creation
+  // return resposne
+
+  const { username, fullName, password, email } = req.body;
+  if (
+    [fullName, password, email, username].some((field) => field?.trim === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
   });
+
+  if (existedUser) {
+    throw new ApiError(400, "User already exists");
+  }
+
+  const avatarLocalPath = req.files.avatar[0].path;
+  const coverImageLocalPath = req.files.coverImage[0].path;
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+  if (!avatar) {
+    throw new ApiError(404, "Avatar is required");
+  }
+
+  const user = User.create({
+    fullName,
+    username,
+    email,
+    password,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "something went wrong creating user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered successfully"));
 });
 
 export default registerController;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,21 @@
 import registerController from "../controllers/user.controller.js";
 import { Router } from "express";
+import upload from "../middlewares/multer.middleware.js";
 
 const userRouter = Router();
 
-userRouter.route("/register").post(registerController);
+userRouter.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerController
+);
 
 export default userRouter;
